ab894dfa5a96eeb015063c96ced6cbf8dcbe1582
Kaitlyn Anderson <kaitlyn.anderson@uncorkedstudios.com>
2017-06-23 15:59:45 -0700

    buttons set up but still needs secondary activities and individual layouts

--- BottomSheetActivity.java
+++ BottomSheetActivity.java
@@ -1,9 +1,18 @@
 package com.example.kaitlynanderson.coordinatorlayoutbehavior;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
 /**
  * Created by kaitlynanderson on 6/23/17.
  */
 
-public class BottomSheetActivity {
+public class BottomSheetActivity extends AppCompatActivity {
 
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bottom_sheet);
+    }
 }
--- MainActivity.java
+++ MainActivity.java
@@ -1,13 +1,42 @@
 package com.example.kaitlynanderson.coordinatorlayoutbehavior;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements Button.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button bottomSheetButton = (Button) findViewById(R.id.button_coordinator1);
+        bottomSheetButton.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch(view.getId()) {
+            case R.id.button_coordinator1:
+                Intent intent = new Intent(this, BottomSheetActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.button_coordinator2:
+                break;
+            case R.id.button_coordinator3:
+                break;
+            case R.id.button_percent_relative_layout:
+                break;
+            case R.id.button_percent_to_constraint:
+                break;
+            case R.id.button_constraint_chain:
+                break;
+            case R.id.button_all_layouts:
+                break;
+        }
     }
 }
