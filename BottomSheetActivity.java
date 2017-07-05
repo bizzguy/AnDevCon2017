80f1f4af951c902f7285512584075f7ad9e67864
Kaitlyn Anderson <kaitlyn.anderson@uncorkedstudios.com>
2017-07-04 20:12:08 -0700

    First example activity created

--- BottomSheetActivity.java
+++ BottomSheetActivity.java
@@ -1,18 +1,72 @@
 package com.example.kaitlynanderson.coordinatorlayoutbehavior;
 
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomSheetBehavior;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.HashMap;
 
 /**
  * Created by kaitlynanderson on 6/23/17.
+ * Activity showing how a basic Bottom Sheet works. Demonstrates a Coordinator Layout behavior
+ * and how that might work right out of the box.
  */
 
 public class BottomSheetActivity extends AppCompatActivity {
 
+    private BottomSheetBehavior mBottomSheetBehavior;
+
+    private TextView mSheetOffsetText;
+
+    private TextView mSheetStateText;
+
+    private HashMap<Integer, String> mStateMap;
+
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_bottom_sheet);
+
+        mStateMap = new HashMap<>();
+        mStateMap.put(1, "DRAGGING");
+        mStateMap.put(2, "SETTLING");
+        mStateMap.put(3, "EXPANDED");
+        mStateMap.put(4, "COLLAPSED");
+        mStateMap.put(5, "HIDDEN");
+
+        LinearLayout bottomSheet = (LinearLayout) findViewById(R.id.container_bottom_sheet);
+
+        mSheetOffsetText = (TextView) findViewById(R.id.text_drawer_offset);
+        mSheetStateText = (TextView) findViewById(R.id.text_sheet_state);
+        mBottomSheetBehavior = BottomSheetBehavior.from(bottomSheet);
+        mBottomSheetBehavior.setHideable(false); // so that it cannot be swiped away
+        mBottomSheetBehavior.setPeekHeight(
+                getResources().getDimensionPixelSize(R.dimen.bottom_sheet_peek_height));
+        mBottomSheetBehavior.setBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
+            @Override
+            public void onStateChanged(@NonNull View bottomSheet, int newState) {
+                String stateText = "Current state is: " + mStateMap.get(newState);
+                mSheetStateText.setText(stateText);
+            }
+
+            @Override
+            public void onSlide(@NonNull View bottomSheet, float slideOffset) {
+                String offsetText = "Sheet Offset is: " + String.valueOf(slideOffset);
+                mSheetOffsetText.setText(offsetText);
+            }
+        });
+        mBottomSheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
+        String stateTextInitial = "Current state is: " + mStateMap
+                .get(mBottomSheetBehavior.getState());
+        mSheetStateText.setText(stateTextInitial);
+        String sheetOffsetInitial = "Sheet Offset is: 1.0";
+        mSheetOffsetText.setText(sheetOffsetInitial);
+
+
     }
 }
--- MainActivity.java
+++ MainActivity.java
@@ -6,21 +6,35 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity implements Button.OnClickListener {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
 
         Button bottomSheetButton = (Button) findViewById(R.id.button_coordinator1);
-        bottomSheetButton.setOnClickListener(this);
+        Button customBottomSheetButton = (Button) findViewById(R.id.button_coordinator2);
+        Button collapsingToolbarButton = (Button) findViewById(R.id.button_coordinator3);
+        Button percentRelativeLayout = (Button) findViewById(R.id.button_percent_relative_layout);
+        Button percentToConstraintButton = (Button) findViewById(R.id.button_percent_to_constraint);
+        Button constraintChainButton = (Button) findViewById(R.id.button_constraint_chain);
+        Button allTheLayoutsButton = (Button) findViewById(R.id.button_all_layouts);
 
+        bottomSheetButton.setOnClickListener(this);
+        customBottomSheetButton.setOnClickListener(this);
+        collapsingToolbarButton.setOnClickListener(this);
+        percentRelativeLayout.setOnClickListener(this);
+        percentToConstraintButton.setOnClickListener(this);
+        constraintChainButton.setOnClickListener(this);
+        allTheLayoutsButton.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
-        switch(view.getId()) {
+        int id = view.getId();
+        switch(id) {
             case R.id.button_coordinator1:
                 Intent intent = new Intent(this, BottomSheetActivity.class);
                 startActivity(intent);
